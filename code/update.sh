7e5162c863829537773ed826814a9b43aae79f46
Anielkis Herrera <aherrera@zato.io>
2019-12-21 10:00:42 -0500

    GH #959 - Adding detection of Git parameter

--- code/update.sh
+++ code/update.sh
@@ -5,7 +5,13 @@ while([ -h "${CURDIR}" ]) do CURDIR=`$RL "${CURDIR}"`; done
 N="/dev/null";pushd .>$N;cd `dirname ${CURDIR}`>$N;CURDIR=`pwd`;popd>$N
 
 echo "*** Downloading updates ***"
-git -C $CURDIR pull
+if [[ -n "$(git --help|grep '\-C')" ]]; then
+    git -C $CURDIR pull
+else
+    pushd $CURDIR
+    git pull
+    popd
+fi
 
 # Uninstall old dependencies
 $CURDIR/bin/pip uninstall -y sec-wall
@@ -15,4 +21,3 @@ $CURDIR/bin/pip uninstall -y zato-apitest
 echo "*** Installing updates ***"
 $CURDIR/bin/pip install -e $CURDIR/zato-cy
 $CURDIR/bin/pip install -r $CURDIR/requirements.txt
-
